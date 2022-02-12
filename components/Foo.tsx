843a624a637888ff4fea841b035902c6e85a02d3
wtlin1228 <wtlin1228@gmail.com>
2022-02-13 00:03:32 +0800

    feat: add useHello hook with a test example

--- components/Foo.tsx
+++ components/Foo.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import useHello from '../hooks/useHello'
 
 const Title = styled.h1`
   font-size: 1.5em;
@@ -13,10 +14,28 @@ const Wrapper = styled.section`
 `
 
 const Foo = () => {
+  const helloQuery = useHello()
+
+  const renderHello = () => {
+    if (helloQuery.isError) {
+      return <span>{helloQuery.error.message}</span>
+    }
+
+    if (helloQuery.isSuccess) {
+      return (
+        <div>
+          <span>{helloQuery.data.name}</span>
+          <span>Calvert</span>
+        </div>
+      )
+    }
+  }
+
   // Use Title and Wrapper like any other React component – except they're styled!
   return (
     <Wrapper>
       <Title>Hello World!</Title>
+      {renderHello()}
     </Wrapper>
   )
 }
--- components/__tests__/Foo.test.tsx
+++ components/__tests__/Foo.test.tsx
@@ -1,7 +1,25 @@
-import { render } from '@testing-library/react'
+import { rest } from 'msw'
+import { render, screen, server } from '../../test-utils'
 
 import Foo from '../Foo'
 
 it('renders without crash', () => {
   render(<Foo />)
+})
+
+it('renders Andy and Calvert', async () => {
+  server.use(
+    rest.get('api/hello', (_req, res, ctx) => {
+      return res(
+        ctx.json({
+          name: 'Andy You',
+        })
+      )
+    })
+  )
+
+  render(<Foo />)
+
+  await screen.findByText('Andy You')
+  expect(screen.getByText('Calvert')).toBeTruthy()
 })
