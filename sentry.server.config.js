d1e7d6ef2ce38a05a58f82bc41653432a8b9328d
wtlin1228 <wtlin1228@gmail.com>
2022-02-13 18:48:59 +0800

    chore: remove default sentry dsn

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,9 +7,7 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    'https://2e1740d1fafe4e048728dcf72e7fc750@o1142604.ingest.sentry.io/6201632',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
