fc0357fd33126dc0d4031dc36ce03c115009a44e
wtlin1228 <wtlin1228@gmail.com>
2022-02-15 13:02:54 +0800

    refactor: extract makeHttpRequest to support different HTTP methods

--- utils/http.ts
+++ utils/http.ts
@@ -1,31 +1,41 @@
 // Note:
 //   Assume the api responses follow this schema:
 //   object {
-//     object {...}* data?;
+//     object {
+//      ...
+//     }* data?;
 //     object {
 //       integer code?;
 //       string message?;
 //     }* error?;
 //   }*;
-export async function httpGet<T>(
-  input: RequestInfo,
-  init?: RequestInit
-): Promise<T> {
-  const response = await fetch(input, init)
 
-  let json
-  try {
-    json = await response.json()
-  } catch {
-    // 5xx
-    throw new Error('Something went wrong in the server.')
+export function makeHttpRequest(method: 'GET' | 'POST' | 'PUT' | 'PATCH') {
+  return async function httpRequest<T>(
+    input: RequestInfo,
+    init?: RequestInit
+  ): Promise<T> {
+    const response = await fetch(input, { method, ...init })
+
+    let json
+    try {
+      json = await response.json()
+    } catch {
+      // 5xx
+      throw new Error('Something went wrong in the server.')
+    }
+
+    if (!response.ok) {
+      // 4xx
+      throw new Error(json.error.message)
+    }
+
+    // 2xx
+    return json.data
   }
-
-  if (!response.ok) {
-    // 4xx
-    throw new Error(json.error.message)
-  }
-
-  // 2xx
-  return json.data
 }
+
+export const httpGet = makeHttpRequest('GET')
+export const httpPOST = makeHttpRequest('POST')
+export const httpPUT = makeHttpRequest('PUT')
+export const httpPATCH = makeHttpRequest('PATCH')
